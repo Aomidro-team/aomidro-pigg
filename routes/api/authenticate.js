3a59bf2e9b5d34d902724cdc1088bbbb5a456e66
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-05-14 12:51:27 +0900

    Create layout's base.

--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -11,7 +11,6 @@ router.post('/', (req, res) => {
 
   // TODO: change to connecting DB
   if (user.username === 'hoge' && user.password === 'hoge') {
-    const secret = 'fuga';
     const token = jwt.sign(user, config.get('authenticate').secret, { expiresIn: '2h' });
 
     res.json({ token });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'AomidroPigg' });
+  res.render('index', { title: 'アオミドロピグ' });
 });
 
 module.exports = router;
