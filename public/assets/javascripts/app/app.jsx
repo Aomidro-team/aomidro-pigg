069baba82344bf22773ed2978a93bb9eff29c632
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-05-26 02:18:45 +0900

    Introduce the environment of redux, redux-saga. fixed #17

---
+++ public/assets/javascripts/app/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import App from './containers/App';
+
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
+
+// <IndexRoute component={Dashboard} />
+// <Route path="/*" />
